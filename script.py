6f801fa283f1f5c18102fafe2a82015ace181961
Ferdi Akdogan <ferdiakdogan@outlook.com>
2019-07-15 02:51:06 +0300

    Story is finished.

--- script.py
+++ script.py
@@ -19,8 +19,6 @@ class TreeNode:
                 chosen_child = story_node.choices[chosen_index]
                 print(chosen_child.story_piece)
                 story_node = chosen_child
-
-
             else:
                 "Invalid choice, enter 1 or 2: "
 
@@ -60,8 +58,39 @@ Do you:
 2 ) Explain that the bear scared you.
 """)
 
+choice_a_1 = TreeNode("""
+The bear returns and tells you it's been a rough week. After making peace with
+a talking bear, he shows you the way out of the forest.
+
+YOU HAVE ESCAPED THE WILDERNESS.
+""")
+
+choice_a_2 = TreeNode("""
+The bear returns and tells you that bullying is not okay before leaving you alone
+in the wilderness.
+
+YOU REMAIN LOST.
+""")
+
+choice_b_1 = TreeNode("""
+The bear is unamused. After smelling the flowers, it turns around and leaves you alone.
+
+YOU REMAIN LOST.
+""")
+
+choice_b_2 = TreeNode("""
+The bear understands and apologizes for startling you. Your new friend shows you a 
+path leading out of the forest.
+
+YOU HAVE ESCAPED THE WILDERNESS.
+""")
+
 story_root.add_child(choice_a)
 story_root.add_child(choice_b)
+choice_a.add_child(choice_a_1)
+choice_a.add_child(choice_a_2)
+choice_b.add_child(choice_b_1)
+choice_b.add_child(choice_b_2)
 story_root.traverse()
 
 
